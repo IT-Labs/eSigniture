2d57dc3c274d1d26eb55f2b55aa9ce65379f77ae
zpedev <zoran.petrusev@gmail.com>
2021-06-04 15:33:47 +0200

    DocuSign client poc initial commit

---
+++ DocuSign/launcher-csharp/eSignature/Models/EmbeddedSigningModel.cs
@@ -0,0 +1,8 @@
+﻿namespace DocuSign.CodeExamples.Views
+{
+    public class EmbeddedSigningModel
+    {
+        public string SignerEmail { get; set; }
+        public string SignerName { get; set; }
+    }
+}
